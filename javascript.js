0b10e8ede8ca62e0fd7a4089af49a23b86f03b2b
Xumanth <juturusumanth23@gmail.com>
2025-07-01 20:24:21 +0530

    Create new 'p' element for displaying scorecard
    Now, textContent will not override the winner declaration with scorecard

--- javascript.js
+++ javascript.js
@@ -22,60 +22,53 @@ let computerScore = 0;
 function playRound(event){
     const x = event.target.id
     const y = getComputerChoice();
-    const div = document.querySelector("#scoreboard") 
+    const div = document.querySelector("#scoreboard");
+    const text = document.createElement("p");
     if(x == 'paper' && y == 'scissors') {
-        div.textContent = `You lose! ${y} beats ${x}`;
-        div.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
         computerScore += 1;
+        text.textContent = `You lose! ${y} beats ${x}`;
+        div.appendChild(text);
+        const text2 = document.createElement("p");
+        text2.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
+        div.appendChild(text2);
     }
     else if (x < y) {
-        div.textContent = `You win! ${x} beats ${y}`;
-        div.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
         humanScore += 1;
+        text.textContent = `You win! ${x} beats ${y}`;
+        div.appendChild(text);
+        const text2 = document.createElement("p");
+        text2.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
+        div.appendChild(text2);
     }
     else if (x > y) {
-        div.textContent = `You lose! ${y} beats ${x}`;
-        div.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
         computerScore += 1;
+        text.textContent = `You lose! ${y} beats ${x}`;
+        div.appendChild(text);
+        const text2 = document.createElement("p");
+        text2.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
+        div.appendChild(text2);
     }
     else {
-        div.textContent = 'Tie';
-        div.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
+        text.textContent = 'Tie';
+        div.appendChild(text);
+        const text2 = document.createElement("p");
+        text2.textContent = `Your Score: ${humanScore} Computer Score ${computerScore}`;
+        div.appendChild(text2);
     }
     if (humanScore==5 || computerScore==5) {
+        const text3 = document.createElement("p");
         if (humanScore==5) {
-            div.textContent = "So, You are the Final Winner";
+            text3.textContent = "So, You are the Final Winner";
+            div.appendChild(text3);
         }
         else {
-            div.textContent = "So, Computer is the Final Winner";
+            text3.textContent = "So, Computer is the Final Winner";
+            div.appendChild(text3);
         }
+        humanScore = 0;            
+        computerScore = 0;
     }
 }
-
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-//     let humanChoice = getHumanchoice;
-//     let computerChoice = getComputerChoice;
-//     for (i=0; i<5; i++) {
-//         let result = playRound(humanChoice(), computerChoice());
-//         if(result == 1) {
-//             humanScore += 1;
-//         }
-//         else if(result == 0) {
-//             computerScore += 1;
-//         }
-//     }
-//     console.log(`Your Score at the end of the game is '${humanScore}'`);
-//     console.log(`Computer Score at the end of the game is '${computerScore}'`);
-//     if (humanScore > computerScore) {
-//         return "So, You are the Final Winner";
-//     }
-//     else {
-//         return "So, Computer is the Final Winner";
-//     }
-// }
-// console.log(playGame());
 
 const btnList = document.querySelectorAll("button");
 btnList.forEach( (button) => {
